f2c918a2b3799fe6e4862b391d9b4ff47ac1210f
Brian <brianriveros14@gmail.com>
2020-07-19 18:57:54 -0400

    projecto

---
+++ src/project/configs.py
@@ -0,0 +1,5 @@
+class Config:
+    # protocolo://usuario:password@host:puerto/basededatos
+    SQLALCHEMY_DATABASE_URI = 'postgres://briveros:optativo123@35.224.193.212:5432/briveros'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET = '123456'
